b6252ee18fffb2cfa09bc6a790ee6962f63c7977
Justine Leleu <justine.leleu.28@gmail.com>
2023-11-06 10:54:26 +0100

    init project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hackers-poulette
+
+figma: https://www.figma.com/file/klWLNqdhZAAsJL4yBm6Zmd/Hackers-Poulette?type=design&node-id=0-1&mode=design&t=DUTUNA9uXdmHDhcV-0
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js,php}'],
+  theme: {
+    extend: 
+    {
+      fontFamily: 
+      {
+        bellota: ["Bellota", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
